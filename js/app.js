3e2d4f2c896bb5ee73375cd5fe15933b3b8bbbac
upoison <destivengarzon@gmail.com>
2022-05-04 23:38:52 -0500

    New changes in the progress Bar

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 const header = document.querySelector("header");
 
+const first_skill = document.querySelector(".skill:first-child");
+const sk_counters = document.querySelectorAll(".counter span");
+const progress_bars = document.querySelectorAll(".skills svg");
+
+window.addEventListener("scroll", () =>{
+    skillCounter();
+});
+
 /* ------------------ Sticky Navbar -------------- */
 
 function stickyNavbar() {
@@ -19,3 +27,9 @@ let sr = ScrollReveal({
 
 sr.reveal(".showcase-info", {delay: 600});
 sr.reveal(".showcase-image", { origin: "top", delay: 700});
+
+/* ------------------ Skills Progress Bar Animation -------------- */
+
+function skillCounter(){
+    console.log("You've reached the skills section");
+}
